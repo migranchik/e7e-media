57ed149d5919d235fa52613dcfbab2d64edae7af
migranchik <63667278+migranchik@users.noreply.github.com>
2022-09-17 07:46:47 +0500

    host new site

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+document.querySelector('.header__burger').addEventListener('click', function (){
+    document.querySelector('.header__burger').classList.toggle('active');
+    document.querySelector('.navigation').classList.toggle('active');
+})
